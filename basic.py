b465991b42984016580b0549cad2389654e8c56a
Onyeka Nwamba <onyeka@Onyekas-Nwamba1-MacBook-Pro.local>
2018-11-22 15:33:37 +0000

    Numbers and expressions

--- basic.py
+++ basic.py
@@ -1,4 +1,5 @@
 from sys import *
+import re
 
 tokens = []
 
@@ -6,13 +7,17 @@ tokens = []
 def open_file(filename):
     #print(filename)
     data = open(filename, "r").read()
+    date = data + "<EOF>"
     return data
 
 
 def lex(fileContents):
+    isExpr = 0
     tok = ""
     state = 0
     string = ""
+    expr = ""
+    n = ""
     fileContents = list(fileContents)
     for char in fileContents:
         tok +=char
@@ -21,10 +26,25 @@ def lex(fileContents):
                 tok = ""
             else:
                 tok = " "
-        elif tok == "\n":
+        elif tok == "\n" or tok == "<EOF>":
+            if expr !="" and isExpr == 1:
+                tokens.append("EXPR:" + expr)
+                expr = ""
+            elif expr !="" and isExpr == 0:
+                #we know its a number
+                tokens.append("NUM:" + expr)
+                expr = ""
             tok = ""
         elif tok == "print":
             tokens.append("PRINT")
+            tok = ""
+        elif tok =="0" or tok=="1" or tok=="2" or tok=="3" or tok=="4" or tok=="5" or tok=="6" or tok=="7" or tok=="8" or tok=="9":
+            print("NUMBER:")
+            expr = expr + tok
+            tok = ""
+        elif tok == "+":
+            isExpr = 1
+            expr = expr + tok
             tok = ""
         elif tok == "\"":
             if state == 0:
@@ -37,6 +57,7 @@ def lex(fileContents):
         elif state == 1:
             string +=tok
             tok = ""
+    print(expr)
     return tokens
     #print(tokens)
 
