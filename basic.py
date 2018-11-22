7726c5a5e338a3e8ceeef4486c72774c0d7cd4a4
Onyeka Nwamba <onyeka@Onyekas-Nwamba1-MacBook-Pro.local>
2018-11-22 01:03:06 +0000

    Outputting strings

--- basic.py
+++ basic.py
@@ -24,26 +24,34 @@ def lex(fileContents):
         elif tok == "\n":
             tok = ""
         elif tok == "print":
-            tokens.append("print")
+            tokens.append("PRINT")
             tok = ""
         elif tok == "\"":
             if state == 0:
                 state = 1
             elif state == 1:
-                tokens.append("string:" + string + "\"")
+                tokens.append("STRING:" + string + "\"")
                 string = ""
                 state = 0
                 tok = ""
         elif state == 1:
             string +=tok
             tok = ""
-    print(tokens)
+    return tokens
+    #print(tokens)
 
+def parse(toks):
+    i = 0
+    while(i<len(toks)):
+        if toks[i] + " " + toks[i + 1][0:6] == "PRINT STRING":
+            print(toks[i+1][7:])
+            i = i + 2
 
 def run():
     open_file(argv[1])
     data = open_file(argv[1])
-    lex(data)
+    toks = lex(data)
+    parse(toks)
 
 
 run()
